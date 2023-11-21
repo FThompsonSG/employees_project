8f94c479133024f4014888d657a76ccbaab7caec
Fergus Thompson <FThompson@spartaglobal.com>
2023-11-21 15:46:23 +0000

    Created dao branch and DaoTest.java

---
+++ src/test/java/com/sparta/javajive/view/DaoTest.java
@@ -0,0 +1,23 @@
+package com.sparta.javajive.view;
+
+import org.junit.jupiter.api.Test;
+
+class DaoTest {
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getByLastName() {
+    }
+
+    @Test
+    void getByDateRange() {
+    }
+
+    @Test
+    void getByAgeRange() {
+    }
+
+}
