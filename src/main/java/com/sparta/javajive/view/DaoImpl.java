f5a0aa82b715327ca1d62269bacbac14616ea576
Fergus Thompson <FThompson@spartaglobal.com>
2023-11-21 16:51:49 +0000

    Created DaoImpl class implementing Dao, amended layout in DaoTest

---
+++ src/main/java/com/sparta/javajive/view/DaoImpl.java
@@ -0,0 +1,27 @@
+package com.sparta.javajive.view;
+
+import java.util.ArrayList;
+
+public class DaoImpl implements Dao {
+
+    @Override
+    public Employee getById(String id) {
+        return null;
+    }
+
+    @Override
+    public Employee getByLastName(String lastName) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Employee> getByDateRange(String startDate, String endDate) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Employee> getByAgeRange(String startAge, String endAge) {
+        return null;
+    }
+
+}
--- src/test/java/com/sparta/javajive/view/DaoTest.java
+++ src/test/java/com/sparta/javajive/view/DaoTest.java
@@ -1,17 +1,24 @@
 package com.sparta.javajive.view;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 class DaoTest {
 
+    DaoImpl daoImpl = new DaoImpl();
+    static ArrayList<Employee> employees = new ArrayList<>();
+
     public static void set() {
-        ArrayList<Employee> employees;
         Employee employee1 = new Employee("11111", "Mr", "Fergus", "R", "Thompson", "M", "example1@hotmail.com", "14/03/1991", "06/11/2023", "£19,000");
         Employee employee2 = new Employee("22222", "Mr", "Fergus", "R", "Kavanagh", "M", "example1@hotmail.com", "14/03/1993", "11/07/2022", "£19,000");
         Employee employee3 = new Employee("33333", "Mr", "Fergus", "R", "Cunha", "M", "example1@hotmail.com", "14/03/1996", "25/02/2020", "£19,000");
         Employee employee4 = new Employee("44444", "Mr", "Fergus", "R", "Boorman", "M", "example1@hotmail.com", "14/03/1999", "19/04/2018", "£19,000");
+        employees.add(employee1);
+        employees.add(employee2);
+        employees.add(employee3);
+        employees.add(employee4);
     }
 
     @BeforeAll
@@ -20,19 +27,20 @@ class DaoTest {
     }
 
     @Test
-    void getById() {
+    void getByIdTest() {
+        Assertions.assertEquals(employees[0], daoImpl.getById("11111"));
     }
 
     @Test
-    void getByLastName() {
+    void getByLastNameTest() {
     }
 
     @Test
-    void getByDateRange() {
+    void getByDateRangeTest() {
     }
 
     @Test
-    void getByAgeRange() {
+    void getByAgeRangeTest() {
     }
 
 }
