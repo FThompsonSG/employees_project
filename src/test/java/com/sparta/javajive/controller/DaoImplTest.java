518fa2870fae8eb9873e2a47e6f6f17b5f8df45b
Fergus Thompson <FThompson@spartaglobal.com>
2023-11-23 16:39:35 +0000

    Wrote passing test for getByLastName

--- src/test/java/com/sparta/javajive/controller/DaoImplTest.java
+++ src/test/java/com/sparta/javajive/controller/DaoImplTest.java
@@ -1,13 +1,16 @@
-package com.sparta.javajive.view;
+package com.sparta.javajive.controller;
 
 import com.sparta.javajive.controller.DaoImpl;
 import com.sparta.javajive.model.Employee;
+import com.sparta.javajive.model.EmployeeFactory;
 import com.sparta.javajive.model.EmployeeStore;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class DaoImplTest {
 
@@ -16,15 +19,9 @@ class DaoImplTest {
 
     @BeforeAll
     public static void set() {
-//        Employee employee1 = new Employee(11111, "Mr", "Fergus", 'R', "Thompson", "M", "example1@hotmail.com", "14/03/1991", "06/11/2023", 19000);
-//        Employee employee2 = new Employee(22222, "Mr", "Fergus", 'C', "Kavanagh", "M", "example1@hotmail.com", "14/03/1993", "11/07/2022", 20000);
-//        Employee employee3 = new Employee(33333, "Mr", "Fergus", 'R', "Cunha", "M", "example1@hotmail.com", "14/03/1996", "25/02/2020", 19000);
-//        Employee employee4 = new Employee(44444, "Mr", "Fergus", 'C', "Boorman", "M", "example1@hotmail.com", "14/03/1999", "19/04/2018", 19000);
-//        employees.add(employee1);
-//        employees.add(employee2);
-//        employees.add(employee3);
-//        employees.add(employee4);
         EmployeeStore.initializeEmployeeStore(10);
+        Employee employee = new Employee(11111, "Mr", "Fergus", 'R', "Thompson", "M", "example1@hotmail.com", "14/03/1991", "06/11/2023", 19000);
+        EmployeeStore.addEmployee(employee);
     }
 
     @BeforeAll
@@ -41,6 +38,13 @@ class DaoImplTest {
     void getByLastNameNoneFoundTest() {
         Employee employee = new Employee(777872, "Mr.", "Blaine", 'Q', "Cockrell", "M", "blaine.cockrell@gmail.com", "31/08/1973", "29/06/2016", 176446);
         Assertions.assertEquals(employees, daoImpl.getByLastName("Cockrell"));
+    }
+
+    @Test
+    void getByLastNameTest() {
+        Employee employee = new Employee(11111, "Mr", "Fergus", 'R', "Thompson", "M", "example1@hotmail.com", "14/03/1991", "06/11/2023", 19000);
+        employees.add(employee);
+        Assertions.assertEquals(employees.toString(), daoImpl.getByLastName("Thompson").toString());
     }
 
     @Test
--- src/test/java/com/sparta/javajive/model/countCorruptedFieldsTest.java
+++ src/test/java/com/sparta/javajive/model/countCorruptedFieldsTest.java
@@ -1,2 +1,46 @@
-package com.sparta.javajive.model;public class countCorruptedFieldsTest {
+package com.sparta.javajive.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class countCorruptedFieldsTest {
+    CorruptedEmployeeChecker checker = new CorruptedEmployeeChecker();
+    String[] thisGuyString = {"155554419","Mr.","Bob","J","Thatcher","NULL","bobthatcher.com","151212","5151211","-1000"};
+    Employee thisGuyEmployeeObject = new Employee(155554419,"Mr.","Bob",'J',"Thatcher","NULL","bobthatcher.com","151212","5151211",-1000);
+    @Test
+    public void corruptedDatesTest(){
+        Assertions.assertEquals(2, checker.checkCorruptedDates(thisGuyString));
+        Assertions.assertEquals(2, checker.checkCorruptedDates(thisGuyEmployeeObject));
+    }
+
+    @Test
+    public void corruptedEmailTest(){
+        Assertions.assertEquals(1, checker.checkCorruptedEmail(thisGuyString));
+        Assertions.assertEquals(1, checker.checkCorruptedEmail(thisGuyEmployeeObject));
+    }
+
+    @Test
+    public void corruptedGenderTest(){
+        Assertions.assertEquals(1, checker.checkCorruptedGender(thisGuyString));
+        Assertions.assertEquals(1, checker.checkCorruptedGender(thisGuyEmployeeObject));
+    }
+
+    @Test
+    public void corruptedIdTest(){
+
+        Assertions.assertEquals(1, checker.checkCorruptedId(thisGuyString));
+        Assertions.assertEquals(1, checker.checkCorruptedId(thisGuyEmployeeObject));
+    }
+
+    @Test
+    public void corruptedSalaryTest(){
+        Assertions.assertEquals(1, checker.checkCorruptedSalary(thisGuyString));
+        Assertions.assertEquals(1, checker.checkCorruptedSalary(thisGuyEmployeeObject));
+    }
+
+    @Test
+    public void totalTest(){
+        Assertions.assertEquals(6, checker.countCorruptedFields(thisGuyString));
+        Assertions.assertEquals(6, checker.countCorruptedFields(thisGuyEmployeeObject));
+    }
 }
