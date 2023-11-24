2cd996fc1d1aa135a5ccec2537a031d5e673d62a
Craig Woodcock <cwoodcock@spartaglobal.com>
2023-11-24 14:51:55 +0000

    added try/catch to all UserInput methods

--- src/main/java/com/sparta/javajive/view/UserInput.java
+++ src/main/java/com/sparta/javajive/view/UserInput.java
@@ -4,6 +4,7 @@ import com.sparta.javajive.controller.DaoImpl;
 import com.sparta.javajive.model.Employee;
 import com.sparta.javajive.model.EmployeeStore;
 
+import java.nio.channels.ScatteringByteChannel;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -33,29 +34,41 @@ public class UserInput {
         }
     }
 
-    public void lastNameScanner() {
-        Scanner lastNameScanner = new Scanner(System.in);
-        System.out.println("Please enter the Last Name");
-        String lastNameInput = lastNameScanner.nextLine();
-        System.out.println(daoImpl.getByLastName(lastNameInput));
+    public void lastNameScanner() throws InputMismatchException{
+        try {
+            Scanner lastNameScanner = new Scanner(System.in);
+            System.out.println("Please enter the Last Name");
+            String lastNameInput = lastNameScanner.nextLine();
+            System.out.println(daoImpl.getByLastName(lastNameInput));
+        }catch (InputMismatchException e){
+            System.err.println("---------------Invalid Input!--------------- \nPlease Enter Employee ID as a Numeric Value" );
+        }
     }
 
-    public void dateRangeScanner() {
-        Scanner dateRangeScanner = new Scanner(System.in);
-        System.out.println("Please enter the first date in date range: (dd/MM/YYYY)");
-        String dateRangeFirstInput = dateRangeScanner.nextLine();
-        System.out.println("Please enter the second date in date range: (dd/MM/YYYY)");
-        String dateRangeLastInput = dateRangeScanner.nextLine();
-        PrintArrayList.printArrayList(daoImpl.getByDateRange(dateRangeFirstInput, dateRangeLastInput));
+    public void dateRangeScanner() throws InputMismatchException{
+        try {
+            Scanner dateRangeScanner = new Scanner(System.in);
+            System.out.println("Please enter the first date in date range: (dd/MM/YYYY)");
+            String dateRangeFirstInput = dateRangeScanner.nextLine();
+            System.out.println("Please enter the second date in date range: (dd/MM/YYYY)");
+            String dateRangeLastInput = dateRangeScanner.nextLine();
+            PrintArrayList.printArrayList(daoImpl.getByDateRange(dateRangeFirstInput, dateRangeLastInput));
+        }catch (InputMismatchException e){
+            System.err.println("---------------Invalid Input!---------------\n Please Enter Date in Format dd/MM/YYYY" );
+        }
     }
 
-    public void ageRangeScanner() {
-        Scanner ageRangeScanner = new Scanner(System.in);
-        System.out.println("Please enter the first age in age range: ");
-        String ageRangeFirstInput = ageRangeScanner.nextLine();
-        System.out.println("Please enter the second age in age range: ");
-        String ageRangeLastInput = ageRangeScanner.nextLine();
-        PrintArrayList.printArrayList(daoImpl.getByAgeRange(ageRangeFirstInput, ageRangeLastInput));
+    public void ageRangeScanner() throws InputMismatchException {
+        try{
+            Scanner ageRangeScanner = new Scanner(System.in);
+            System.out.println("Please enter the first age in age range: ");
+            String ageRangeFirstInput = ageRangeScanner.nextLine();
+            System.out.println("Please enter the second age in age range: ");
+            String ageRangeLastInput = ageRangeScanner.nextLine();
+            PrintArrayList.printArrayList(daoImpl.getByAgeRange(ageRangeFirstInput, ageRangeLastInput));
+        }catch(InputMismatchException e){
+            System.err.println("---------------Invalid Input!---------------\n   Please Enter Age as a Numeric Value" );
+        }
     }
 
 }
